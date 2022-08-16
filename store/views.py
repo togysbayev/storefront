94ebccaac0c89075a2dd67059d556747f0fdedf1
Bekarys Togysbayev <togysbayev.bekarys@gmail.com>
2022-08-17 01:58:59 +0600

    Generic filtering for products

--- store/views.py
+++ store/views.py
@@ -6,22 +6,20 @@ from rest_framework.response import Response
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
 from django.db.models import Count
 from rest_framework import status
 from .models import Product, Collection, Review
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 from store import serializers
+from .filters import ProductFilter
 
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
-    def get_queryset(self):
-        queryset = Product.objects.select_related('collection').all()
-        collection_id = self.request.query_params.get('collection_id')
-        if collection_id:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
     
     def destroy(self, request, *args, **kwargs):
         product = get_object_or_404(Product, pk=kwargs['pk'])
